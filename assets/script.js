d147bfd1860227e14eaa53b6c34182ba839f9d39
Matthew M. Provost <provostmat@gmail.com>
2022-12-07 20:11:16 -0500

    created files and added structure to html

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+var start_btn = document.querySelector(".start_btn button");
+var quiz = document.querySelector(".quiz");
+var intro = document.querySelector(".intro");
+var start_btn = document.querySelector(".start_btn button");
+
+
+
+
+
+
+
+
+var questions[0] = {
+    question: "Question",
+    choices: [x,x,x,x],
+    correctAnswer: x
+}
+
+
+start_btn.addEventListener('click', function() {
+    intro.display.setAttribute('opacity', 0);
+})
+
+
+
+function correctAnswer(event) {
+
+    for (var i = 0; i < questions.length; i++)
+        if ()
+
+}
+
+
+start_btn.addEventListener('click', startQuiz);
+correctAnswer.addEventListener('click', correctAnswer);
